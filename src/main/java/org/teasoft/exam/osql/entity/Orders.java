49fd001c324112a85e05237eeed8bc49ecb656fc
Kingstar <honeysoft@126.com>
2020-02-28 17:24:13 +0800

    add entity

---
+++ src/main/java/org/teasoft/exam/osql/entity/Orders.java
@@ -0,0 +1,102 @@
+package org.teasoft.exam.osql.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+/**
+*@author Honey
+*Create on 2020-02-28 17:18:48
+*/
+public class Orders implements Serializable {
+
+	private static final long serialVersionUID = 1591834367922L;
+
+	private Long id;
+	private String userid;
+	private String name;
+	private BigDecimal total;
+	private Timestamp createtime;
+	private String remark;
+	private String sequence;
+	private String abc;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getTotal() {
+		return total;
+	}
+
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
+
+	public Timestamp getCreatetime() {
+		return createtime;
+	}
+
+	public void setCreatetime(Timestamp createtime) {
+		this.createtime = createtime;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	public String getSequence() {
+		return sequence;
+	}
+
+	public void setSequence(String sequence) {
+		this.sequence = sequence;
+	}
+
+	public String getAbc() {
+		return abc;
+	}
+
+	public void setAbc(String abc) {
+		this.abc = abc;
+	}
+
+	 public String toString(){	
+		 StringBuffer str=new StringBuffer();	
+		 str.append("Orders[");			
+		 str.append("id=").append(id);		 
+		 str.append(",userid=").append(userid);		 
+		 str.append(",name=").append(name);		 
+		 str.append(",total=").append(total);		 
+		 str.append(",createtime=").append(createtime);		 
+		 str.append(",remark=").append(remark);		 
+		 str.append(",sequence=").append(sequence);		 
+		 str.append(",abc=").append(abc);		 
+		 str.append("]");			 
+		 return str.toString();			 
+	 }		 
+}

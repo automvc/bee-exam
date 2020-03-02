57a88c9ad031c5c738adf3a449efa9a1951f0d84
Bee <honeysoft@126.com>
2020-03-02 22:29:33 +0800

    add

---
+++ src/main/java/org/teasoft/exam/comm/TestHelper.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2016-2020 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.exam.comm;
+
+import org.teasoft.honey.osql.core.Logger;
+
+/**
+ * @author Kingstar
+ * @since  1.7.2
+ */
+public class TestHelper {
+	
+	public static void markStart(String startMsg){
+		Logger.info("[bee-exam] ===================="+startMsg+"======start:");
+	}
+	
+	public static void markEnd(String endMsg){
+		Logger.info("[bee-exam] ===================="+endMsg+"======end.\n");
+	}
+	
+	
+	public static void runTest(Class c) {
+		
+		try {
+//			c.getName()
+			String simpleName=c.getSimpleName();
+			markStart(simpleName+".test");
+			c.getMethod("test", null).invoke(null, null);
+//		           如果底层方法是静态的，那么可以忽略指定的 obj 参数。该参数可以为 null。 
+			markEnd(simpleName+".test");
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+}

03815ed6fe5b7bb5816491c4b342988ffee1a884
Bee <honeysoft@126.com>
2020-03-02 22:31:29 +0800

    add example.

---
+++ src/main/java/org/teasoft/exam/bee/osql/SuidExamEN.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2016-2020 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+package org.teasoft.exam.bee.osql;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.teasoft.bee.osql.BeeException;
+import org.teasoft.bee.osql.Suid;
+import org.teasoft.exam.bee.osql.entity.Orders;
+import org.teasoft.honey.osql.core.BeeFactory;
+import org.teasoft.honey.osql.core.Logger;
+
+/**
+ * @author Kingstar
+ * @since  1.0
+ */
+public class SuidExamEN {
+	
+	public static void main(String[] args) {
+		test();
+	}
+	public static void test() {
+		
+	  try{
+		Suid suid=BeeFactory.getHoneyFactory().getSuid();
+		
+		Orders orders1=new Orders();
+		orders1.setId(100001L);
+		orders1.setName("Bee--ORM Framework");
+		
+		List<Orders> list1 =suid.select(orders1);  //select
+		for (int i = 0; i < list1.size(); i++) {
+			Logger.info(list1.get(i).toString());
+		}
+		
+		orders1.setName("Bee--ORM Framework");
+		int updateNum=suid.update(orders1);   //update
+		Logger.info("update record:"+updateNum);
+		
+		Orders orders2=new Orders();
+		orders2.setUserid("bee");
+		orders2.setName("Bee-ORM framework");
+		orders2.setTotal(new BigDecimal("91.99"));
+		orders2.setRemark("");  //empty String test
+		
+		int insertNum=suid.insert(orders2); //insert
+		Logger.info("insert record:"+insertNum);
+		
+		int deleteNum=suid.delete(orders2);   //delete
+		Logger.info("delete record:"+deleteNum);
+		
+		List<Orders> list2 =suid.select(orders1); //select  confirm the data
+		for (int i = 0; i < list2.size(); i++) {
+			Logger.info(list2.get(i).toString());
+		}
+		
+	} catch (BeeException e) {
+		e.printStackTrace();
+	}
+	}
+
+}

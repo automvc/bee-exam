af839a0f4839c56cbe289e3b68bd085113e1f600
Bee <honeysoft@126.com>
2020-03-02 22:41:18 +0800

    update example.

--- src/main/java/org/teasoft/exam/bee/osql/ConditionExam.java
+++ src/main/java/org/teasoft/exam/bee/osql/ConditionExam.java
@@ -15,32 +15,37 @@ import org.teasoft.bee.osql.Suid;
 import org.teasoft.exam.bee.osql.entity.Orders;
 import org.teasoft.honey.osql.core.BeeFactory;
 import org.teasoft.honey.osql.core.ConditionImpl;
+import org.teasoft.honey.osql.core.Logger;
 
 /**
  * @author Kingstar
  * @since  1.6
  */
 public class ConditionExam {
+	
 	public static void main(String[] args) {
+		test();
+	}
+	public static void test() {
 
 		Suid suid = BeeFactory.getHoneyFactory().getSuid();
 		Orders orders = new Orders();
 		
-		orders.setName("KS"); //等于的条件,会默认转换
+		orders.setName("Bee(ORM framework)"); //等于的条件,会默认转换
 //		orders.setTotal(new BigDecimal("100"));  //不会再处理.因为between已有用
 		
 		 Condition condition=new ConditionImpl();
 		 condition
-		 .op("userid", Op.like, "Bee%") //模糊查询
+		 .op("userid", Op.like, "bee%") //模糊查询
 		 .between("total", 90, 100)     //范围查询
-		 .between("createtime","2019-10-01","2019-11-12")
+		 .between("createtime","2020-03-01","2020-03-03")
 		 .orderBy("userid",OrderType.ASC) //排序
 		 .start(0).size(10)              //分页
 		 ;
 
 		List<Orders> list2 = suid.select(orders, condition);
 		for (int i = 0; i < list2.size(); i++) {
-			System.out.println(list2.get(i).toString());
+			Logger.info(list2.get(i).toString());
 		}
 	}
 }

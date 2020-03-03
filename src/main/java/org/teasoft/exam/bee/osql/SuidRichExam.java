d72994e7fe18289698da5c1e23dbd9112f5b5f35
Kingstar <honeysoft@126.com>
2020-03-03 22:56:31 +0800

    add more test

--- src/main/java/org/teasoft/exam/bee/osql/SuidRichExam.java
+++ src/main/java/org/teasoft/exam/bee/osql/SuidRichExam.java
@@ -6,30 +6,174 @@
 
 package org.teasoft.exam.bee.osql;
 
-import org.teasoft.bee.osql.BeeException;
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.teasoft.bee.osql.IncludeType;
 import org.teasoft.bee.osql.SuidRich;
+import org.teasoft.exam.bee.osql.entity.Orders;
 import org.teasoft.exam.bee.osql.entity.User;
 import org.teasoft.honey.osql.core.BeeFactory;
+import org.teasoft.honey.osql.core.Logger;
 
 /**
  * @author Kingstar
  * @since  1.0
  */
 public class SuidRichExam {
-	
+
 	public static void main(String[] args) {
 		test();
 	}
-	
-	public static void test(){
-		try {
-			SuidRich suidRich = BeeFactory.getHoneyFactory().getSuidRich();
-			suidRich.selectById(new User(), "800001,800002");
-			suidRich.selectById(new User(), 800001);
-			suidRich.selectById(new User(), 800001L);
-		} catch (BeeException e) {
-			e.printStackTrace();
+
+	public static void test() {
+
+		
+		SuidRich suidRich = BeeFactory.getHoneyFactory().getSuidRich();
+		
+		Orders orders=new Orders();
+		orders.setName("client");
+		orders.setUserid("client");
+		orders.setRemark("testOneTime");
+		orders.setTotal(new BigDecimal("93.99"));
+		
+		orders.setSequence("");  //empty String test
+		
+		
+		int insertNum1=suidRich.insert(orders,IncludeType.INCLUDE_NULL);
+		Logger.info("insert record:"+insertNum1);
+		
+		int insertNum2=suidRich.insert(orders,IncludeType.INCLUDE_EMPTY);
+		Logger.info("insert record:"+insertNum2);
+		
+		int insertNum3=suidRich.insert(orders,IncludeType.INCLUDE_BOTH);
+		Logger.info("insert record:"+insertNum3);
+		
+		
+		int updateNum1=suidRich.update(orders, "name,remark");
+		Logger.info("updateNum1:  "+updateNum1);
+		
+		int updateNum2=suidRich.update(orders, "name");
+		Logger.info("updateNum2:  "+updateNum2);
+		
+//		ObjSQLException: updateFieldList at least include one field.
+//		int updateNum3=suidRich.update(orders, "");
+//		Logger.info("updateNum3:  "+updateNum3);
+		
+		int updateNum4=suidRich.update(orders, "name",IncludeType.INCLUDE_EMPTY);
+		Logger.info("updateNum4:  "+updateNum4);
+		
+		int updateNum5=suidRich.update(orders, "name",IncludeType.INCLUDE_NULL);
+		Logger.info("updateNum5:  "+updateNum5);
+		
+		int updateNum6=suidRich.update(orders, "name",IncludeType.INCLUDE_BOTH);
+		Logger.info("updateNum6:  "+updateNum6);
+		
+		int updateNum4_2=suidRich.update(orders, "name,total",IncludeType.INCLUDE_EMPTY);
+		Logger.info("updateNum4_2:  "+updateNum4_2);
+		
+		int updateNum5_2=suidRich.update(orders, "name,total",IncludeType.INCLUDE_NULL);
+		Logger.info("updateNum5_2:  "+updateNum5_2);
+		
+		int updateNum6_2=suidRich.update(orders, "name,total",IncludeType.INCLUDE_BOTH);
+		Logger.info("updateNum6_2:  "+updateNum6_2);
+		
+		//the id field of entity must not be null !
+//		int updateNum8_0=suidRich.update(orders,IncludeType.INCLUDE_EMPTY);
+//		Logger.info("updateNum8_0:  "+updateNum8_0);
+		
+		
+		Orders orders00=new Orders();
+		
+		orders00.setId(100L);
+		orders00.setName("client01");
+		orders00.setUserid("client00");
+		orders00.setRemark("testOneTime");
+		orders00.setTotal(new BigDecimal("93.99"));
+		
+		orders00.setRemark("");  //empty String test
+		
+		
+		int insertNum00=suidRich.insert(orders00,IncludeType.INCLUDE_NULL);
+		Logger.info("insert record:"+insertNum00);
+		
+		int updateNum8=suidRich.update(orders00,IncludeType.INCLUDE_EMPTY);
+		Logger.info("updateNum8:  "+updateNum8);
+		int updateNum9=suidRich.update(orders00,IncludeType.INCLUDE_NULL); //TODO
+		Logger.info("updateNum9:  "+updateNum9);
+		int updateNum10=suidRich.update(orders00,IncludeType.INCLUDE_BOTH); //TODO
+		Logger.info("updateNum10:  "+updateNum10);
+		
+		
+		int deleteNum=suidRich.delete(orders00);
+		System.out.println("deleteNum: "+deleteNum);
+		
+		//test batch insert
+		Orders orders0=new Orders();
+		orders0.setId(1007L);
+		orders0.setUserid("client01");
+//		orders0.setName("MVC book");
+		orders0.setTotal(new BigDecimal(91));
+		orders0.setRemark("testOneTime");
+		
+		Orders orders1=new Orders();
+		orders1.setId(1008L);
+		orders1.setUserid("client02");
+		orders1.setRemark("testOneTime");
+		orders1.setTotal(new BigDecimal(20));
+		
+		Orders orders2=new Orders();
+		orders2.setId(1009L);
+		orders2.setUserid("client03");
+		orders2.setRemark("testOneTime");
+		orders2.setTotal(new BigDecimal(23));
+
+		
+		Orders[] ordersArray=new Orders[3];
+		ordersArray[0]=orders0;
+		ordersArray[1]=orders1;
+		ordersArray[2]=orders2;
+		
+//		int insertArray[]=suidRich.insert(ordersArray,2);
+		int insertArray[]=suidRich.insert(ordersArray,2,"id,datetime");//batch insert
+		for (int i = 0; i < insertArray.length; i++) {
+			Logger.info(insertArray[i]+" ,");
 		}
+		
+		int insertArray2[]=suidRich.insert(ordersArray,"id,datetime");//batch insert
+		for (int i = 0; i < insertArray2.length; i++) {
+			Logger.info(insertArray2[i]+" ,");
+		}
+		
+		
+		Orders exampleField=new Orders();
+		exampleField.setUserid("bee");
+//		
+		List<Orders> listSome=suidRich.select(exampleField, "name,total");
+		Logger.info(listSome.size()+"");
+		
+		Orders ordersForString=new Orders();
+		ordersForString.setUserid("client01");
+//		List<String[]> listString=
+		suidRich.selectString(ordersForString);
+		List<String[]> listString=suidRich.selectString(ordersForString,"id,userid,total,createtime");
+		for (String str[]:listString) {
+//			String str[]=listString.get(i);
+			for (String s:str) {
+				System.out.print(s+"  , ");
+			}
+			Logger.info("");
+		}
+		
+		Orders order_del=new Orders();
+		order_del.setRemark("testOneTime");
+		int deleteNum2=suidRich.delete(order_del);
+		System.out.println("deleteNum2: "+deleteNum2);
+		
+
+		suidRich.selectById(new User(), "800001,800002");
+		suidRich.selectById(new User(), 800001);
+		suidRich.selectById(new User(), 800001L);
 	}
 
 }

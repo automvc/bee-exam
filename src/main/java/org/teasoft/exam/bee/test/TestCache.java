c1a4c352de54c405446264e66057687cb24b9bfa
Kingstar <honeysoft@126.com>
2020-03-04 11:06:18 +0800

    add test/example

---
+++ src/main/java/org/teasoft/exam/bee/test/TestCache.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2016-2020 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.exam.bee.test;
+
+import org.teasoft.exam.bee.osql.CacheTest;
+import org.teasoft.exam.bee.osql.CacheTestDel;
+import org.teasoft.exam.bee.osql.CacheTestInsertArray;
+import org.teasoft.exam.bee.osql.CacheTestJson;
+import org.teasoft.exam.bee.osql.ConditionExam;
+import org.teasoft.exam.bee.osql.PreparedSqlExam;
+import org.teasoft.exam.bee.osql.SuidExam;
+import org.teasoft.exam.bee.osql.SuidExamEN;
+import org.teasoft.exam.bee.osql.SuidRichExam;
+import org.teasoft.exam.bee.osql.TransactionExam;
+import org.teasoft.exam.bee.osql.UpdateByExam;
+import org.teasoft.exam.comm.TestHelper;
+import org.teasoft.exam.comm.TestPrepare;
+import org.teasoft.honey.osql.util.DateUtil;
+
+/**
+ * @author Kingstar
+ * @since  1.7.2
+ */
+public class TestCache {
+	
+	public static void main(String[] args) {
+		TestPrepare.init("cache");
+		
+	
+		
+		runTest(CacheTest.class);
+		runTest(CacheTestDel.class);
+		runTest(CacheTestInsertArray.class);
+		runTest(CacheTestJson.class);
+		
+		
+		System.out.println(DateUtil.currentDate()+"  test cache Finished!");
+		
+		
+	}
+	
+	public static void runTest(Class c) {
+		TestHelper.runTest(c);
+	}
+
+}

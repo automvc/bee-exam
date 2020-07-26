4f9b2b7aa78323b02927812d977ced9704e4c294
Kingstar <honeysoft@126.com>
2020-07-26 22:58:22 +0800

    使用#{entityName?up1}可以首字母大写

--- src/main/java/org/teasoft/exam/bee/osql/autogen/GenFilesExam.java
+++ src/main/java/org/teasoft/exam/bee/osql/autogen/GenFilesExam.java
@@ -19,12 +19,12 @@ public class GenFilesExam {
 	public static void main(String[] args) {
 		
 		Map<String, String> map = new HashMap<>();
-		map.put("entityName1", "Orderhistory");
+//		map.put("entityName1", "Orderhistory");  //使用#{entityName?up1}可以首字母大写
 		map.put("entityName", "orderhistory");
 		map.put("packageName", "com.automvc.enet.order.rest");
-		更改成本地的具体路径  change to your real path
-//		String basePath = "D:\\JavaWeb\\workspaceGit\\bee-exam\\src\\main\\java\\org\\teasoft\\exam\\bee\\osql\\autogen\\";
-		String basePath = "D:\\xxx\\yyy\\bee-exam\\src\\main\\java\\org\\teasoft\\exam\\bee\\osql\\autogen\\";
+//		更改成本地的具体路径  change to your real path
+		String basePath = "D:\\JavaWeb\\workspaceGit\\bee-exam\\src\\main\\java\\org\\teasoft\\exam\\bee\\osql\\autogen\\";
+//		String basePath = "D:\\xxx\\yyy\\bee-exam\\src\\main\\java\\org\\teasoft\\exam\\bee\\osql\\autogen\\";
 		
 		String templatePath = basePath + "OrderhistoryRest.java.template";
 		String targetFilePath = basePath + "OrderhistoryRest.java";

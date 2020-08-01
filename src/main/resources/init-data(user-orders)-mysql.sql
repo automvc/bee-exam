22cf710e4ae4a6b51976e03a2db682c219becc55
Kingstar <honeysoft@126.com>
2020-08-01 13:44:06 +0800

    update the sql script

--- src/main/resources/init-data(user-orders)-mysql.sql
+++ src/main/resources/init-data(user-orders)-mysql.sql
@@ -58,3 +58,28 @@ CREATE TABLE `user` (
 -- ----------------------------
 INSERT INTO `user` VALUES ('800001', 'beeUser@163.com', null, 'Bee', 'bee', 'bee', null);
 INSERT INTO `user` VALUES ('800002', 'honey@163.com', null, 'Honey', 'honey', 'honey', '2020-03-02 16:41:33');
+
+-- ----------------------------
+-- Table structure for `leaf_alloc`
+-- ----------------------------
+DROP TABLE IF EXISTS `leaf_alloc`;
+CREATE TABLE `leaf_alloc` (
+  `id` bigint(11) NOT NULL,
+  `biz_tag` varchar(128) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `max_id` bigint(20) NOT NULL DEFAULT '1',
+  `step` int(11) NOT NULL,
+  `description` varchar(256) COLLATE utf8_bin DEFAULT NULL,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `version` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_index_biz_tag` (`biz_tag`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Records of leaf_alloc
+-- id(456537470402562) has global unique featrue.
+-- see: org.teasoft.honey.distribution.SerialUniqueId
+-- ----------------------------
+INSERT INTO `leaf_alloc` VALUES ('456537470402562', 'test', '100', '100', 'test segment', '2020-07-04 22:42:09', '1');
+INSERT INTO `leaf_alloc` VALUES ('456537470402563', 'order', '100', '100', 'order segment', '2020-07-04 22:42:10', '1');
+

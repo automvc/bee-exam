6ed020cc2376fc8f8cf313ee5981d5156ac62ad3
Kingstar <honeysoft@126.com>
2020-09-26 13:43:34 +0800

    update

--- src/main/java/org/teasoft/exam/bee/distribution/PearFlowerIdTest.java
+++ src/main/java/org/teasoft/exam/bee/distribution/PearFlowerIdTest.java
@@ -7,7 +7,7 @@
 package org.teasoft.exam.bee.distribution;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -21,15 +21,16 @@ import org.teasoft.honey.osql.core.Logger;
 public class PearFlowerIdTest {
 	
 	public static void main(String[] args) {
-		Set<Long> set=new HashSet<>();
+		Set<Long> set=new LinkedHashSet<>();
 		List list=new ArrayList();
 		for (int i = 0; i < 1000000; i++) {
 			if(i%10==0){
-				long ids[] =GenIdFactory.getRangeId("userTable",100);
+				long ids[] =GenIdFactory.getRangeId("userTable",85);
 				for (long j = ids[0]; j <= ids[1]; j++) {
 //					Logger.info(j);
 					if(!set.add(j)) list.add(j);
 				}
+//				System.out.println(set);
 			}else{
 			long id =GenIdFactory.get("userTable");
 			Logger.info(id);

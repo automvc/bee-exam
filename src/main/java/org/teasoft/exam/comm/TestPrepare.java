3d53158b879a3ae17fb0b4580e12b73682a9f6b5
Kingstar <honeysoft@126.com>
2020-10-17 21:54:05 +0800

    v1.8.99

--- src/main/java/org/teasoft/exam/comm/TestPrepare.java
+++ src/main/java/org/teasoft/exam/comm/TestPrepare.java
@@ -20,7 +20,8 @@ public class TestPrepare {
 //	static String version="v1.7.2";
 //	static String version="v1.7.3";
 //	static String version="v1.8";
-	static String version="v1.8.15";
+//	static String version="v1.8.15";
+	static String version="v1.8.99";
 	
 	public static void init(){
 		init("");

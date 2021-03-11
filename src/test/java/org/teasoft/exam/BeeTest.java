bf690fc1ee45d6f92209625be301e361fd2abc54
Kingstar <honeysoft@126.com>
2021-03-11 08:05:39 +0800

    enhance test for V1.9

--- src/test/java/org/teasoft/exam/BeeTest.java
+++ src/test/java/org/teasoft/exam/BeeTest.java
@@ -8,6 +8,8 @@ package org.teasoft.exam;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.teasoft.exam.bee.osql.autogen.GenBeanExam;
+import org.teasoft.exam.bee.osql.autogen.GenFilesExam;
 import org.teasoft.exam.bee.test.TestCache;
 import org.teasoft.exam.bee.test.TestNormal;
 
@@ -25,7 +27,10 @@ public class BeeTest {
 		boolean result=false;
 		try {
 			TestNormal.test();
-//			TestCache.test();
+			TestCache.test();
+			
+			GenBeanExam.test();
+			GenFilesExam.test();
 			
 			result=true;
 		} catch (Exception e) {

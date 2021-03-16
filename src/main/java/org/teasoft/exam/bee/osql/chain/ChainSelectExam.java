f73a8478eb5edc237a05922088a330faff2f4660
Kingstar <honeysoft@126.com>
2021-03-16 14:05:15 +0800

    add test chain select

---
+++ src/main/java/org/teasoft/exam/bee/osql/chain/ChainSelectExam.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2016-2021 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.exam.bee.osql.chain;
+
+import java.util.List;
+
+import org.teasoft.bee.osql.Op;
+import org.teasoft.bee.osql.OrderType;
+import org.teasoft.bee.osql.PreparedSql;
+import org.teasoft.bee.osql.chain.Select;
+import org.teasoft.exam.comm.Printer;
+import org.teasoft.honey.osql.chain.Aggregate;
+import org.teasoft.honey.osql.chain.SelectImpl;
+import org.teasoft.honey.osql.core.BeeFactory;
+import org.teasoft.honey.osql.core.Logger;
+
+/**
+ * @author Kingstar
+ * @since  1.9
+ */
+public class ChainSelectExam {
+	private static PreparedSql preparedSql = BeeFactory.getHoneyFactory().getPreparedSql();
+	public static void main(String[] args) {
+		Select t = new SelectImpl();
+		String sql = "";
+//      t.select("*")
+        t
+//        .select("id")
+        .distinct("id")
+        .select(Aggregate.max("total"))
+//        .select("max(price)")
+//        .distinct("name")
+		.from("orders")
+//		.from("abc") //如何检测已经使用了from,不能再用了??
+		.where()
+//		.op("userid", Op.eq, "client01")
+//		.and()  //default will add
+//		.op("name", Op.like, "Bee%")  要考虑注入攻击
+//		.op("name", Op.like, "test2_")
+		.op("name", Op.notLike, "Bee%") //自己拼字符,占位形式不能自动
+//		.op("category", Op.in, "food,fruit")
+		.between("total", 10, 100)
+//		.notBetween("total", 90, 100)
+		.isNotNull("createtime")
+        
+        .groupBy("name")
+//        .orderBy("name")
+        .orderBy("id", OrderType.ASC);
+        
+        
+//      sql= t.from("abc") //重新用t写,就可以从原来的开始.
+        sql= t.toSQL();
+        List<String[]> list1= preparedSql.select(sql);
+        Printer.print(list1);
+		
+		Select goodOrders = new SelectImpl();
+//		goodOrders.distinct("id")
+		goodOrders
+		.select("id,name,total")
+//		.max("total")
+		.from("orders")
+		.where()
+		.op("name", Op.eq, "client")
+		.or()
+		.op("name", Op.eq, "bee")
+		;
+		
+		goodOrders.groupBy("name")
+		.having("total>avg(total)")
+//		.count("testtest")   顺序没能控制好
+		;
+        List<String[]> list2= preparedSql.select(goodOrders.toSQL());
+        Printer.print(list2);
+		
+		Select whereJoin = new SelectImpl();
+//		book.distinct("name")
+		whereJoin
+		.select("orders.id,total,orders.name,test_user.name")  //查多表时，怎么知道这是要查哪个表的字段？？  字段带表名，返回的结果如何赋值给Javabean?  要指明字段是哪个表的,可用别名。
+//		.max("total")  
+		.from("orders,test_user")
+		.where("orders.name=test_user.name")
+//		.where()
+		.lParentheses()
+		.between("total", 0, 100)
+		.rParentheses()
+//		.lParentheses()
+		.op("orders.name", Op.eq, "Bee")
+//		.rParentheses()
+		;
+		
+        List<String[]> list3= preparedSql.select(whereJoin.toSQL());
+        Printer.print(list3);
+		
+//		Op opType=Op.notIn;
+//		if (opType == Op.notIn) System.out.println("===== true");
+		
+//		Select subSelect=new Select();
+//		subSelect 
+		Select c=new SelectImpl();
+		c.select("team_id")
+		.from("teams")
+		.groupBy("team_id")
+		.having("count(*)=sum(case when status='aaa' then 1 else 0 end)");
+		
+		System.out.println(c.toSQL());
+		
+		Select join =new SelectImpl();
+		join.select("*")
+		.from("orders o")
+		.join("test_user t")
+		.on("o.name=t.name");
+		
+        List<String[]> list5= preparedSql.select(join.toSQL());
+        Printer.print(list5);
+        
+        
+		String classno = "201";
+		int page = 1, rows = 10;
+    	Select selectSql = new SelectImpl();
+		selectSql.select("id,examno,classno,name,teacherno,createtime,updatetime")
+		.from("scores")
+		.where()
+//		.op("classno", Op.eq, classno)
+		.op("classno", Op.in, classno) 
+		.start((page - 1) * rows)
+		.size(rows)
+		//			.select("count(*)")              
+		//TODO  要是.toSQL后,才开始生成sql,那该类,就有可能重用. 只改select部分即可.
+		;
+        List<String[]> list6= preparedSql.select(selectSql.toSQL());
+        Printer.print(list6);
+
+		Select selectSql2 = new SelectImpl();
+		selectSql2.select("count(*)")
+		.from("scores")
+		.where()
+//		.op("classno", Op.eq, classno) //  可以扩展 Op.or实现
+		.op("classno", Op.in, classno) 
+		;
+		
+        List<String[]> list7= preparedSql.select(selectSql2.toSQL());
+        Printer.print(list7);
+        
+        
+        
+        Select wherePart=new SelectImpl();
+        wherePart
+//		.where()
+		.lParentheses()
+		.between("total", 0, 100)
+		.rParentheses()
+//		.lParentheses()
+		.op("orders.name", Op.eq, "Bee")
+//		.rParentheses()
+		;
+        
+        Logger.info(wherePart.toSQL());
+		
+	}
+
+}
---
+++ src/main/java/org/teasoft/exam/comm/Printer.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2016-2021 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.exam.comm;
+
+import java.util.List;
+
+import org.teasoft.honey.osql.core.Logger;
+import org.teasoft.honey.util.ObjectUtils;
+
+/**
+ * @author Kingstar
+ * @since  1.9
+ */
+public class Printer {
+
+	public static void print(List<String[]> list) {
+		if (ObjectUtils.isEmpty(list)) return;
+		String a[] = null;
+		String row = null;
+		for (int i = 0; list != null && i < list.size(); i++) {
+			a = list.get(i);
+			row = "";
+			for (int j = 0; j < a.length; j++) {
+				row += a[j] + "   ";
+			}
+			Logger.info(row);
+		}
+	}
+
+}

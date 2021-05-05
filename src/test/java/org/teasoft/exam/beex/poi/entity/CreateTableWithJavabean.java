c9661f247287f1924b05edab556c19900b569053
Kingstar <honeysoft@126.com>
2021-05-05 20:59:00 +0800

    update for V1.9

---
+++ src/test/java/org/teasoft/exam/beex/poi/entity/CreateTableWithJavabean.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2016-2021 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.exam.beex.poi.entity;
+
+import org.teasoft.honey.osql.autogen.Ddl;
+
+/**
+ * @author Kingstar
+ * @since  1.9
+ */
+public class CreateTableWithJavabean {
+	public static void main(String[] args) {
+		Ddl.createTable(new LeftszInfo(), true);
+	}
+
+}

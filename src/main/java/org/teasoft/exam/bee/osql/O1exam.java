9fcec31ca52acb42a363a5fd9f8530bdcdacb237
Kingstar <honeysoft@126.com>
2021-09-08 01:03:05 +0800

    添加O(1)示例.

---
+++ src/main/java/org/teasoft/exam/bee/osql/O1exam.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2016-2021 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.exam.bee.osql;
+
+import java.util.List;
+
+import org.teasoft.bee.osql.Suid;
+import org.teasoft.exam.bee.osql.entity.Orders;
+import org.teasoft.exam.bee.osql.entity.Student;
+import org.teasoft.exam.bee.osql.entity.StudentHobby;
+import org.teasoft.honey.osql.core.BeeFactory;
+import org.teasoft.honey.osql.core.Logger;
+
+/**
+ * @author Kingstar
+ * @since  1.9.8
+ */
+public class O1exam {
+	public static void main(String[] args) {
+		Suid suid=BeeFactory.getHoneyFactory().getSuid();
+		
+		Student student=new Student();
+		student.setId(1);
+		List<Student> list1 =suid.select(student); //处理list1
+		for (int i = 0; i < list1.size(); i++) {
+			Logger.info(list1.get(i).toString());
+		}
+		
+		StudentHobby hobby=new StudentHobby();
+		hobby.setStuId("1001");
+		List<StudentHobby> list2 =suid.select(hobby);
+		for (int i = 0; i < list2.size(); i++) { //处理list2
+			Logger.info(list2.get(i).toString());
+		}
+	}
+
+}

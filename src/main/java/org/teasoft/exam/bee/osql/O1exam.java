eec7d000fc73c02b52b103ee5d52671a39a19c0f
Kingstar <honeysoft@126.com>
2021-09-26 10:39:51 +0800

    update comment

--- src/main/java/org/teasoft/exam/bee/osql/O1exam.java
+++ src/main/java/org/teasoft/exam/bee/osql/O1exam.java
@@ -25,14 +25,14 @@ public class O1exam {
 		
 		Student student=new Student();
 		student.setId(1);
-		List<Student> list1 =suid.select(student); //处理list1
-		for (int i = 0; i < list1.size(); i++) {
+		List<Student> list1 =suid.select(student); //查询实体student
+		for (int i = 0; i < list1.size(); i++) {//处理list1
 			Logger.info(list1.get(i).toString());
 		}
 		
 		StudentHobby hobby=new StudentHobby();
 		hobby.setStuId("1001");
-		List<StudentHobby> list2 =suid.select(hobby);
+		List<StudentHobby> list2 =suid.select(hobby);//查询实体StudentHobby
 		for (int i = 0; i < list2.size(); i++) { //处理list2
 			Logger.info(list2.get(i).toString());
 		}

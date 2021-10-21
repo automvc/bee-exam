9fe7066310d2dd3a5208646a74389a1563db15b6
Kingstar <honeysoft@126.com>
2021-10-21 21:56:28 +0800

    add for V1.9.8

---
+++ src/test/java/org/teasoft/honey/util/StreamUtilTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2016-2021 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.honey.util;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.junit.Test;
+import org.teasoft.honey.osql.core.Logger;
+
+import static org.teasoft.honey.util.StreamUtil.*;
+
+/**
+ * @author Kingstar
+ * @since  1.9.8
+ */
+public class StreamUtilTest {
+
+	@Test
+	public void test() {
+		try {
+			InputStream in = new FileInputStream("D:\\temp\\user3.txt");
+			System.out.println(stream2String(in));
+		} catch (Exception e) {
+			Logger.error(e.getMessage(), e);
+		}
+	}
+
+}

f77c3facae34f8ba5758fa18d00163b0cfb1194a
Kingstar <honeysoft@126.com>
2021-12-08 22:26:12 +0800

    example how to use null with field

---
+++ src/main/java/org/teasoft/exam/bee/osql/special/UseNull.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2016-2021 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.exam.bee.osql.special;
+
+import java.util.List;
+
+import org.teasoft.bee.osql.Condition;
+import org.teasoft.bee.osql.Op;
+import org.teasoft.bee.osql.Suid;
+import org.teasoft.exam.bee.osql.entity.TestUser;
+import org.teasoft.exam.comm.Printer;
+import org.teasoft.honey.osql.core.BeeFactoryHelper;
+//import org.teasoft.honey.osql.shortcut.BF;
+
+/**
+ * @author Kingstar
+ * @since  1.9.8
+ */
+public class UseNull {
+
+	public static void main(String[] args) {
+		test();
+	}
+
+	public static void test() {
+		try {
+//			Suid suid = BF.getSuid();
+			Suid suid = BeeFactoryHelper.getSuid();
+			Condition condition10 = BeeFactoryHelper.getCondition();
+			//where email is null
+			condition10.op("email", Op.eq, null);
+			List<TestUser> list10 = suid.select(new TestUser(), condition10);
+			Printer.printList(list10);
+			
+			//where email is not null
+			Condition condition11 = BeeFactoryHelper.getCondition();
+			condition11.op("email", Op.nq, null);
+//			condition11.op("email", Op.notEqual, null);
+			List<TestUser> list11 = suid.select(new TestUser(), condition11);
+			Printer.printList(list11);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}

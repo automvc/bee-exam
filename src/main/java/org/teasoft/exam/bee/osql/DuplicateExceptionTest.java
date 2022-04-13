1f8aaf7de0f3495a23e1e79d85d80723c96731a7
Kingstar <honeysoft@126.com>
2022-04-13 09:42:45 +0800

    测试单条插入遇到重复键约束, 异常捕获和显示的问题

--- src/main/java/org/teasoft/exam/bee/osql/DuplicateExceptionTest.java
+++ src/main/java/org/teasoft/exam/bee/osql/DuplicateExceptionTest.java
@@ -9,8 +9,11 @@ package org.teasoft.exam.bee.osql;
 import org.teasoft.bee.osql.SuidRich;
 import org.teasoft.exam.bee.osql.entity.TestUser;
 import org.teasoft.honey.osql.core.BeeFactory;
+import org.teasoft.honey.osql.core.HoneyConfig;
+import org.teasoft.honey.osql.core.Logger;
 
 /**
+ * 测试单条插入遇到重复键约束, 异常捕获和显示的问题
  * @author Kingstar
  * @since  1.11
  */
@@ -21,6 +24,17 @@ public class DuplicateExceptionTest {
 		testUser.setId(800001L);
 		suidRich.insert(testUser);
 		suidRich.insert(testUser);
+		suidRich.insert(testUser);
+		
+		HoneyConfig config=HoneyConfig.getHoneyConfig();
+		
+		Logger.warn("----------test not Show ModifyDuplicateException-----------------");
+		config.notShowModifyDuplicateException=true;  //不显示
+		int a=suidRich.insert(testUser);
+		Logger.warn(a);  //0 ,如果有需要可通过返回结果判断
+		
+		Logger.warn("----------test not Catch ModifyDuplicateException-----------------");
+		config.notCatchModifyDuplicateException=true; //不捕获
 		suidRich.insert(testUser);
 	}
 

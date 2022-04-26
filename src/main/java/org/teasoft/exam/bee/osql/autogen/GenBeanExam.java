3638f148862154471510d187a7c70c0802cd06cc
Kingstar <honeysoft@126.com>
2022-04-26 10:21:58 +0800

    update

--- src/main/java/org/teasoft/exam/bee/osql/autogen/GenBeanExam.java
+++ src/main/java/org/teasoft/exam/bee/osql/autogen/GenBeanExam.java
@@ -31,75 +31,12 @@ public class GenBeanExam {
 			config.setGenSerializable(true);
 			
 //			更改成本地的具体路径  change to your real path
-//			config.setBaseDir("D:\\xxx\\yyy\\bee-exam\\src\\main\\java\\");
-			config.setBaseDir("D:\\JavaWeb\\workspaceGit19\\bee-exam\\src\\main\\java\\");
+			config.setBaseDir("D:\\xxx\\yyy\\bee-exam\\src\\main\\java\\");
 //			config.setPackagePath("org.teasoft.exam.bee.osql.entity");
 			config.setPackagePath("org.teasoft.exam.bee.osql.entity2");
-//			config.setPackagePath("org.teasoft.exam.bee.osql.entity.dynamic");
-//			config.setPackagePath("org.teasoft.exam.bee.osql.entity.sqlite");
-//			config.setPackagePath("org.teasoft.exam.bee.osql.entity.postgreSQL");
-//			config.setPackagePath("org.teasoft.exam.bee.osql.entity.h2");
 
 			GenBean genBean = new GenBean(config);
-//			genBean.genSomeBeanFile("leaf_alloc,Orders");
-//			genBean.genSomeBeanFile("Orders,user");
-//			genBean.genSomeBeanFile("POSTGRESQL_TYPE");
-//			genBean.genSomeBeanFile("H2_TYPE");
-			
-//			genBean.genSomeBeanFile("resource,permit,users,role_assign");
-//			genBean.genSomeBeanFile("tabinfo");
-//			genBean.genSomeBeanFile("ienglish");
-//			genBean.genSomeBeanFile("tableinfo");
-//			genBean.genSomeBeanFile("columninfo");
-//			genBean.genSomeBeanFile("user_table");
-//			genBean.genSomeBeanFile("Download");
-//			genBean.genSomeBeanFile("pro_template");
-//			genBean.genSomeBeanFile("sdrecord,userbook");
-//			genBean.genSomeBeanFile("notifylog");
-//			genBean.genSomeBeanFile("sdrecord");
-//			genBean.genSomeBeanFile("orders");
-//			genBean.genSomeBeanFile("role,resource");
-//			genBean.genSomeBeanFile("noid");
-//			genBean.genSomeBeanFile("clazz,student");
-//			genBean.genSomeBeanFile("student_hobby");
-//			genBean.genSomeBeanFile("tableinfo");
-//			genBean.genSomeBeanFile("dinner");
-//			genBean.genSomeBeanFile("Resource");
-//			genBean.genSomeBeanFile("leftsz_info");
-//			genBean.genSomeBeanFile("t_test");
-//			genBean.genSomeBeanFile("t_user");
-//			genBean.genSomeBeanFile("t_leyy_sign");
-//			genBean.genSomeBeanFile("t_transfer_record");
-//			genBean.genSomeBeanFile("Columns,Tables");
-			
-//			genBean.genSomeBeanFile("users");
-//			genBean.genSomeBeanFile("orders");
-//			genBean.genSomeBeanFile("type_test");
-//			genBean.genSomeBeanFile("template_group");
-//			genBean.genSomeBeanFile("pro_template");
-//			genBean.genSomeBeanFile("tableinfo");
-//			genBean.genSomeBeanFile("userbook");
-//			genBean.genSomeBeanFile("shopping_cart"); //No keyspace has been specified. USE a keyspace, or explicitly specify keyspace.tablename
-//			genBean.genSomeBeanFile("store.shopping_cart");
-//			genBean.genSomeBeanFile("store.cassandra_table");
-			
-//			genBean.genSomeBeanFile("student2");
-//			genBean.genSomeBeanFile("column_test");
-//			genBean.genSomeBeanFile("date_test");
-//			genBean.genSomeBeanFile("Json_Value");
-//			genBean.genSomeBeanFile("leaf_alloc");
-//			genBean.genSomeBeanFile("sqlserver_type");
-			
-//			genBean.genSomeBeanFile("dept");
-//			genBean.genSomeBeanFile("test_primary_key");
-					
-//			genBean.genSomeBeanFile("sqlserver_type");
-//			genBean.genSomeBeanFile("H2_type");
-//			genBean.genSomeBeanFile("POSTGRESQL_TYPE");
-			
-			
-//			genBean.genSomeBeanFile("dict_i18n");
-//			genBean.genSomeBeanFile("counter");
+
 			
 //			config.setGenComment(true);
 			config.setGenFieldFile(true);

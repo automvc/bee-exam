18471d38dbda8aee13fa41934c71a61c5a7aba35
Kingstar <honeysoft@126.com>
2022-07-11 19:19:44 +0800

    update for V1.17.8

--- src/test/java/org/teasoft/honey/util/StringUtilsTest.java
+++ src/test/java/org/teasoft/honey/util/StringUtilsTest.java
@@ -9,6 +9,8 @@ package org.teasoft.honey.util;
 import org.junit.Test;
 import org.teasoft.honey.osql.core.Logger;
 
+import static org.teasoft.honey.util.StringUtils.*;
+
 /**
  * @author Kingstar
  * @since  1.11
@@ -50,6 +52,35 @@ public class StringUtilsTest {
 		str = "34";
 		f1 = StringUtils.isContainLetter(str);
 		System.out.println(f1);
+		
+		
+		System.out.println(isInteger(""));
+		System.out.println(isNumber(""));
+		System.out.println(isInteger(" 0"));
+		System.out.println(isNumber(" 0"));
+		
+		System.out.println(isInteger("-"));
+		System.out.println(isNumber("-"));
+		
+		System.out.println(isInteger("+"));
+		System.out.println(isNumber("+"));
+		
+		System.out.println(isInteger("+0"));
+		System.out.println(isNumber("+0"));
+		
+		System.out.println(isInteger("-0"));
+		System.out.println(isNumber("-0"));
+		
+		
+		System.out.println(isInteger("a123456789"));
+		System.out.println(isNumber("a123456789"));
+		
+		Integer a[]= {1,2,3};
+		toCommasString(a);
+		
+		String strArray[]= {"1","2","3"};
+		toCommasString(strArray);
+		
 		
 	}
 

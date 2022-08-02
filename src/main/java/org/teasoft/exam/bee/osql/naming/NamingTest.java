8056e04fcb4a8ef8ae032ac2bc4447c6bc37aed7
Kingstar <honeysoft@126.com>
2022-08-02 11:16:19 +0800

    add naming transfer exam

---
+++ src/main/java/org/teasoft/exam/bee/osql/naming/NamingTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2016-2022 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.exam.bee.osql.naming;
+
+import java.util.List;
+
+import org.teasoft.bee.osql.SuidRich;
+import org.teasoft.exam.comm.Printer;
+import org.teasoft.honey.osql.core.HoneyConfig;
+import org.teasoft.honey.osql.name.DbUpperAndJavaLower;
+import org.teasoft.honey.osql.name.OriginalName;
+import org.teasoft.honey.osql.shortcut.BF;
+
+/**
+ * @author Kingstar
+ * @since  1.17
+ */
+public class NamingTest {
+	private static SuidRich suidRich=BF.getSuidRich();
+	
+	public static void main(String[] args) {
+		
+		List<Orders> list=suidRich.select(new Orders(), 10);
+		Printer.printList(list);
+		
+//		int oldTranslateType=HoneyConfig.getHoneyConfig().naming_translateType;
+		
+		//#since 1.17 同时使用多用不同命名转换
+		HoneyConfig.getHoneyConfig().naming_useMoreTranslateType=true;
+		
+//		Logger.info("===========================naming_translateType=3");
+//		HoneyConfig.getHoneyConfig().naming_translateType=3;  //不起作用,系统级别的只会保留启动后初始的命名转换类
+		suidRich.setNameTranslate(new OriginalName()); //可以在当前对象设置
+		list=suidRich.select(new Orders(), 10);
+		Printer.printList(list);
+		
+//		Logger.info("===========================naming_translateType=4");
+//		HoneyConfig.getHoneyConfig().naming_translateType=4; //不起作用,系统级别的只会保留启动后初始的命名转换类
+		suidRich.setNameTranslate(new DbUpperAndJavaLower()); //可以在当前对象设置
+		list=suidRich.select(new Orders(), 10);
+		Printer.printList(list);
+		suidRich.select(new Orders(), 10);
+		
+//		HoneyConfig.getHoneyConfig().naming_translateType=oldTranslateType;
+	}
+
+}

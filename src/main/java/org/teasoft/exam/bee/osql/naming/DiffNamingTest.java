dda22f283e3ff8bd21111b8c8b8ccb9dd79650d4
Kingstar <honeysoft@126.com>
2022-08-04 16:00:49 +0800

    update test

--- src/main/java/org/teasoft/exam/bee/osql/naming/DiffNamingTest.java
+++ src/main/java/org/teasoft/exam/bee/osql/naming/DiffNamingTest.java
@@ -19,32 +19,37 @@ import org.teasoft.honey.osql.shortcut.BF;
  * @author Kingstar
  * @since  1.17
  */
-public class NamingTest {
-	private static SuidRich suidRich=BF.getSuidRich();
+public class DiffNamingTest {
 	
-	public static void main(String[] args) {
-		
-		List<Orders> list=suidRich.select(new Orders(), 10);
+	private static SuidRich suidRich = BF.getSuidRich();
+
+	public static void main(String[] args) throws Exception {
+		test();
+	}
+
+	public static void test() throws Exception {
+
+		List<Orders> list = suidRich.select(new Orders(), 10);
 		Printer.printList(list);
-		
+
 //		int oldTranslateType=HoneyConfig.getHoneyConfig().naming_translateType;
-		
-		//#since 1.17 同时使用多用不同命名转换
-		HoneyConfig.getHoneyConfig().naming_useMoreTranslateType=true;
-		
+
+		// #since 1.17 同时使用多用不同命名转换
+		HoneyConfig.getHoneyConfig().naming_useMoreTranslateType = true;
+
 //		Logger.info("===========================naming_translateType=3");
 //		HoneyConfig.getHoneyConfig().naming_translateType=3;  //不起作用,系统级别的只会保留启动后初始的命名转换类
-		suidRich.setNameTranslate(new OriginalName()); //可以在当前对象设置
-		list=suidRich.select(new Orders(), 10);
+		suidRich.setNameTranslate(new OriginalName()); // 可以在当前对象设置
+		list = suidRich.select(new Orders(), 10);
 		Printer.printList(list);
-		
+
 //		Logger.info("===========================naming_translateType=4");
 //		HoneyConfig.getHoneyConfig().naming_translateType=4; //不起作用,系统级别的只会保留启动后初始的命名转换类
-		suidRich.setNameTranslate(new DbUpperAndJavaLower()); //可以在当前对象设置
-		list=suidRich.select(new Orders(), 10);
+		suidRich.setNameTranslate(new DbUpperAndJavaLower()); // 可以在当前对象设置
+		list = suidRich.select(new Orders(), 10);
 		Printer.printList(list);
 		suidRich.select(new Orders(), 10);
-		
+
 //		HoneyConfig.getHoneyConfig().naming_translateType=oldTranslateType;
 	}
 

39beeed9c1be8e820cd7c1ff2e920afb90c6ee72
Kingstar <honeysoft@126.com>
2022-08-14 18:43:03 +0800

    add Orders_F

---
+++ src/main/java/entity/field/Orders_F.java
@@ -0,0 +1,22 @@
+package entity.field;
+
+/**
+ * Relative field name for Javabean Orders
+ * @author Honey
+ * Create on 2022-08-12 17:07:08
+ */
+public class Orders_F {
+
+	private Orders_F() {}
+
+	public static final String id = "id";
+	public static final String userid = "userid";
+	public static final String name = "name";
+	public static final String total = "total";
+	public static final String createtime = "createtime";
+	public static final String remark = "remark";
+	public static final String sequence = "sequence";
+	public static final String abc = "abc";
+	public static final String updatetime = "updatetime";
+	public static final String ALL_NAMES = "id,userid,name,total,createtime,remark,sequence,abc,updatetime";
+}

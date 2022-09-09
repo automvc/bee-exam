c4206be1ffdd9db141918d7a20c4def5cdc975d8
Kingstar <honeysoft@126.com>
2022-09-09 15:51:38 +0800

    add JpaAnnoTest

---
+++ src/main/java/org/teasoft/exam/bee/osql/special/jpa/JpaAnnoTest.java
@@ -0,0 +1,23 @@
+///*
+// * Copyright 2016-2022 the original author.All rights reserved.
+// * Kingstar(honeysoft@126.com)
+// * The license,see the LICENSE file.
+// */
+//
+//package org.teasoft.exam.bee.osql.special.jpa;
+//
+//import org.teasoft.bee.osql.Suid;
+//import org.teasoft.honey.osql.shortcut.BF;
+//
+///**
+// * @author Kingstar
+// * @since  1.17
+// */
+//public class JpaAnnoTest {
+//	
+//	public static void main(String[] args) {
+//		Suid suid=BF.getSuid();
+//		suid.select(new Orders());
+//	}
+//
+//}

2cb19c44a1c7a26b7754c1e2ef54b98f9a3876d6
Kingstar <honeysoft@126.com>
2023-03-04 00:27:16 +0800

    GenBeanExam

--- src/main/java/org/teasoft/exam/bee/osql/autogen/GenBeanExam.java
+++ src/main/java/org/teasoft/exam/bee/osql/autogen/GenBeanExam.java
@@ -26,31 +26,40 @@ public class GenBeanExam {
 //			driverName,url,username,password config in bee.properties.
 
 			GenConfig config = new GenConfig();
-			config.setDbName(dbName);
+			config.setDbName(dbName); // v1.17 will auto get from the config
 			config.setGenToString(true);//生成toString方法
 			config.setGenSerializable(true);
 			
 //			更改成本地的具体路径  change to your real path
-			config.setBaseDir("D:\\xxx\\yyy\\bee-exam\\src\\main\\java\\");
-//			config.setPackagePath("org.teasoft.exam.bee.osql.entity");
-			config.setPackagePath("org.teasoft.exam.bee.osql.entity2");
+//			config.setBaseDir("D:\\xxx\\yyy\\bee-exam\\src\\main\\java\\");
+////			config.setPackagePath("org.teasoft.exam.bee.osql.entity");
+//			config.setPackagePath("org.teasoft.exam.bee.osql.entity2");
+//			config.setPackagePath("org.teasoft.exam.bee.osql.entity.dynamic");
+//			config.setPackagePath("org.teasoft.exam.bee.osql.entity.sqlite");
+//			config.setPackagePath("org.teasoft.exam.bee.osql.entity.postgreSQL");
+//			config.setPackagePath("org.teasoft.exam.bee.osql.entity.h2");
 
 			GenBean genBean = new GenBean(config);
-
-			
+		
 //			config.setGenComment(true);
 			config.setGenFieldFile(true);
 			config.setCommentPlace(1);
 			
 			//设置相对Entity的文件夹; 空表示与Entity同一个文件夹
-//			config.setFieldFileRelativeFolder("field"); 
+//			config.setFieldFileRelativeFolder("field"); //默认
 			
 			config.setOverride(true); //是否覆盖原来的文件
 			
-			genBean.genSomeBeanFile("orders");
+//			genBean.genAllBeanFile();  //生成某个库所有表对应的Javabean
 			
+			//mongodb
+			genBean.genSomeBeanFile("test_user");
+
 			
-//			
+//			System.out.println(System.getProperty("user.dir"));
+			
+			//it is easy way.   最简单的用法,所有的都用默认配置.
+			new GenBean().genSomeBeanFile("orderhistory");
 		  } catch (BeeException e) {
 			 e.printStackTrace();
 		  }

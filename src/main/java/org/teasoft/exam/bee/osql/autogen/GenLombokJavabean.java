72e4eb23ec8734551ffc0f1b15fb54d88bbc4e99
Kingstar <honeysoft@126.com>
2023-03-13 00:07:04 +0800

    GenBean support Lombok Javabean

---
+++ src/main/java/org/teasoft/exam/bee/osql/autogen/GenLombokJavabean.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2016-2023 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.exam.bee.osql.autogen;
+
+import org.teasoft.honey.osql.autogen.GenBean;
+import org.teasoft.honey.osql.autogen.GenConfig;
+
+/**
+ * @author Kingstar
+ * @since  2.1
+ */
+public class GenLombokJavabean {
+	
+	public static void main(String[] args) {
+		GenConfig config = new GenConfig();
+		config.setLombokGetter(true);
+		config.setLombokSetter(true);
+		
+//		config.setLombokData(true);
+		
+		config.setGenSerializable(false);
+		
+		GenBean genBean = new GenBean(config);
+		
+		genBean.genSomeBeanFile("orders");
+	}
+
+}

5cc3d9b16ab7b3ab9f9292b96cc7629e5c7ebea6
Kingstar <honeysoft@126.com>
2023-03-26 23:10:49 +0800

    GenBean Simple use

--- src/main/java/org/teasoft/exam/bee/osql/autogen/GenBeanSimple.java
+++ src/main/java/org/teasoft/exam/bee/osql/autogen/GenBeanSimple.java
@@ -31,24 +31,23 @@ public class GenBeanSimple {
 			
 //			更改成本地的具体路径  change to your real path
 //			config.setBaseDir("D:\\xxx\\yyy\\bee-exam\\src\\main\\java\\");
-			config.setBaseDir("D:\\JavaWeb\\workspaceGit19\\bee-exam\\src\\main\\java\\");
-			config.setPackagePath("org.teasoft.exam.bee.osql.entity2");
+//			config.setPackagePath("org.teasoft.exam.bee.osql.entity2");
 
 			GenBean genBean = new GenBean(config);
 
 			//可以使用{实体}_F来引用字段名  
 			//V1.17 支持生成ALL_NAMES属性(同时引用所有字段的名称)
-			config.setGenFieldFile(true);   //生成字段文件, 这样可以避免硬编码引用字段,速度也也比反射快.
+			config.setGenFieldFile(true);   //生成字段文件, 这样可以避免硬编码引用字段,速度也比反射快.
 			//设置相对Entity目录的文件夹名称; 空表示与Entity同一个文件夹
 //			config.setFieldFileRelativeFolder("field"); //默认
 			config.setOverride(true); //是否覆盖原来的文件
 			
-			genBean.genSomeBeanFile("orders");
+//			genBean.genSomeBeanFile("orders");
 //			genBean.genSomeBeanFile("store.shopping_cart"); //cassandra 需要带keyspace (store.)
 			
 			
 			//it is easy way.   最简单的用法,所有的都用默认配置.
-			new GenBean().genSomeBeanFile("test_user");
+			new GenBean().genSomeBeanFile("orderhistory");
 			
 		  } catch (BeeException e) {
 			 e.printStackTrace();

abc35804882fe92040f74f3c9b3358a147ad1264
Kingstar <honeysoft@126.com>
2023-03-26 23:10:34 +0800

    Create Table With Javabean

--- src/main/java/org/teasoft/exam/bee/osql/autogen/CreateTableWithJavabean2.java
+++ src/main/java/org/teasoft/exam/bee/osql/autogen/CreateTableWithJavabean2.java
@@ -6,7 +6,9 @@
 
 package org.teasoft.exam.bee.osql.autogen;
 
-import org.teasoft.exam.bee.osql.entity.Student;
+import org.teasoft.exam.bee.osql.sharding.InitSameDsUtil;
+import org.teasoft.exam.bee.osql.sharding.ShardingInitData;
+import org.teasoft.exam.bee.osql.sharding.TestUsers;
 import org.teasoft.honey.osql.autogen.Ddl;
 
 /**
@@ -15,7 +17,12 @@ import org.teasoft.honey.osql.autogen.Ddl;
  */
 public class CreateTableWithJavabean2 {
 	public static void main(String[] args) {
-		Ddl.createTable(new Student(), false); //测试有Boolean类型,   在oracle生成表
+//		Ddl.createTable(new Student(), false); //测试有Boolean类型,   在oracle生成表
+		
+		ShardingInitData.init();
+		InitSameDsUtil.initDS();
+//		Ddl.createTable(new TestUser(), true); 
+		Ddl.createTable(TestUsers.class, true); 
 	}
 
 }

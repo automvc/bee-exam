7a34d07536b31396392458dba32df3354d9cf801
Kingstar <honeysoft@126.com>
2023-05-15 00:07:21 +0800

    update for V2.1.5.4

---
+++ src/test/java/org/teasoft/test/suite/JustMongodbTest.java
@@ -0,0 +1,23 @@
+///*
+// * Copyright 2016-2023 the original author.All rights reserved.
+// * Kingstar(honeysoft@126.com)
+// * The license,see the LICENSE file.
+// */
+//
+//package org.teasoft.test.suite;
+//
+///**
+// * @author Kingstar
+// * @since  2.1
+// */
+//import org.junit.platform.runner.JUnitPlatform;
+//import org.junit.platform.suite.api.IncludePackages;
+//import org.junit.platform.suite.api.SelectPackages;
+//import org.junit.runner.RunWith;
+//
+//@RunWith(JUnitPlatform.class)
+//@SelectPackages("org.teasoft.exam") 
+//@IncludePackages("org.teasoft.exam.mongodb")
+//public class JustMongodbTest {
+//
+//}

a6254632d8e4a4322f7b14d755e30f5cf691ee13
Kingstar <honeysoft@126.com>
2023-10-05 16:44:37 +0800

    update for V2.1.8

---
+++ src/test/java/org/teasoft/test/suite/CoreTest.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2016-2023 the original author.All rights reserved.
+ * Kingstar(honeysoft@126.com)
+ * The license,see the LICENSE file.
+ */
+
+package org.teasoft.test.suite;
+
+/**
+ * @author Kingstar
+ * @since  2.1
+ */
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.ExcludePackages;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("org.teasoft.exam") 
+//@SelectPackages("org.teasoft.exam,org.teasoft.bee,org.teasoft.honey") 
+@ExcludePackages({"org.teasoft.exam.mongodb","org.teasoft.exam.sharding"})
+public class CoreTest {
+
+}

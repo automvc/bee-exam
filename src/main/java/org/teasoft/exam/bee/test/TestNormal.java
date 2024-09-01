37be16824e3126686f38f91432555679c6167cef
Kingstar <honeysoft@126.com>
2024-09-02 00:20:34 +0800

    update for V2.4.0

--- src/main/java/org/teasoft/exam/bee/test/TestNormal.java
+++ src/main/java/org/teasoft/exam/bee/test/TestNormal.java
@@ -16,6 +16,7 @@ import org.teasoft.exam.bee.osql.DynamicTableExam;
 import org.teasoft.exam.bee.osql.InsertTest;
 import org.teasoft.exam.bee.osql.LockWaitTimeout;
 import org.teasoft.exam.bee.osql.MapSuidExam;
+import org.teasoft.exam.bee.osql.MapSuidExam24;
 import org.teasoft.exam.bee.osql.MoreSQLFunction;
 import org.teasoft.exam.bee.osql.MoreTableExam;
 import org.teasoft.exam.bee.osql.NamingTest;
@@ -33,15 +34,36 @@ import org.teasoft.exam.bee.osql.UpdateByExam;
 import org.teasoft.exam.bee.osql.UpdateSetExam;
 import org.teasoft.exam.bee.osql.UpdateSetExam_SQLite;
 import org.teasoft.exam.bee.osql.UseJson;
+import org.teasoft.exam.bee.osql.chain.Chain;
 import org.teasoft.exam.bee.osql.chain.ChainSelectExam;
+import org.teasoft.exam.bee.osql.chain.ChainSelectExam2;
 import org.teasoft.exam.bee.osql.chain.ChainUpdateExam;
+import org.teasoft.exam.bee.osql.chain.Delete24;
+import org.teasoft.exam.bee.osql.chain.Insert24;
+import org.teasoft.exam.bee.osql.chain.QueryCompare;
+import org.teasoft.exam.bee.osql.chain.QueryCompare2;
+import org.teasoft.exam.bee.osql.chain.Select24;
 import org.teasoft.exam.bee.osql.chain.UnionSelectExam;
+import org.teasoft.exam.bee.osql.chain.Update24;
 import org.teasoft.exam.bee.osql.commomid.IntSerialIdTest;
 import org.teasoft.exam.bee.osql.commomid.NotLongId;
 import org.teasoft.exam.bee.osql.commomid.StringIdTest;
 import org.teasoft.exam.bee.osql.commomid.StringIdUUIDTest;
 import org.teasoft.exam.bee.osql.dialect.MyFeatureTest;
 import org.teasoft.exam.bee.osql.dialect.PagingExam;
+import org.teasoft.exam.bee.osql.ext24.TestBFX;
+import org.teasoft.exam.bee.osql.localdatetime.LocalDateTimeExam;
+import org.teasoft.exam.bee.osql.localdatetime.LocalDateTimeExam2;
+import org.teasoft.exam.bee.osql.localdatetime.LocalDateTimeExam3;
+import org.teasoft.exam.bee.osql.moretable.modify.CreateTable;
+import org.teasoft.exam.bee.osql.moretable.modify.MoreInsert40;
+import org.teasoft.exam.bee.osql.moretable.modify.MoreInsert40_2;
+import org.teasoft.exam.bee.osql.moretable.modify.MoreInsert50;
+import org.teasoft.exam.bee.osql.moretable.modify.MoreInsert60;
+import org.teasoft.exam.bee.osql.moretable.modify.MoreModify;
+import org.teasoft.exam.bee.osql.moretable.modify.MoreUpdate;
+import org.teasoft.exam.bee.osql.moretable.modify.MoreUpdate2;
+import org.teasoft.exam.bee.osql.moretable.modify.MoreUpdate3;
 import org.teasoft.exam.bee.osql.naming.DiffNamingTest;
 import org.teasoft.exam.bee.osql.special.ColumnAnnoTest;
 import org.teasoft.exam.bee.osql.special.LikeTest;
@@ -144,6 +166,7 @@ public class TestNormal {
 		runTest(SelectFun.class);
 //		runTest(More.class);
 		runTest(MapSuidExam.class);
+		runTest(MapSuidExam24.class);
 		
 		runTest(LockWaitTimeout.class);
 		runTest(SuidExamEN_SQLite.class);
@@ -159,6 +182,30 @@ public class TestNormal {
 		runTest(ChainUpdateExam.class); 
 		runTest(ChainSelectExam.class); 
 		runTest(UnionSelectExam.class);
+		runTest(ChainSelectExam2.class);
+		runTest(Insert24.class);
+//		runTest(NotExam.class); //some databse donot support : !(name='client' and abc='client')
+		runTest(QueryCompare.class);
+		runTest(QueryCompare2.class);
+		runTest(Select24.class);
+		runTest(Update24.class);
+		runTest(Chain.class);
+		
+		if (DatabaseConst.MYSQL.equalsIgnoreCase(DbName)) runTest(Delete24.class);
+		
+		runTest(TestBFX.class);
+		
+		runTest(CreateTable.class);
+		runTest(MoreInsert40.class);
+		runTest(MoreInsert40_2.class);
+		runTest(MoreInsert50.class);
+		runTest(MoreInsert60.class);
+		runTest(MoreModify.class);
+		runTest(MoreUpdate.class);
+		runTest(MoreUpdate2.class);
+		runTest(MoreUpdate3.class);
+		runTest(org.teasoft.exam.bee.osql.moretable.update.MoreUpdate2.class);
+		
 		
 //		HoneyConfig.getHoneyConfig().sqlKeyWordCase="upper"; //can not change after running.
 //		runTest(SuidExam.class);
@@ -192,6 +239,12 @@ public class TestNormal {
 		
 //		runTest(KotlinTest.class);
 //		runTest(HoneyConfigReset.class);
+		
+		if (DatabaseConst.ORACLE.equalsIgnoreCase(DbName)) {
+		    runTest(LocalDateTimeExam.class);
+		    runTest(LocalDateTimeExam2.class);
+		    runTest(LocalDateTimeExam3.class);
+		}
 		
 	   } catch (BeeException e) {
 //		   Logger.error(e.getMessage(),e);

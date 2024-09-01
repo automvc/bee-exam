7b2582777b7f7fad5835b17dcb992fbefc6e72f3
Kingstar <honeysoft@126.com>
2024-09-02 00:19:54 +0800

    update for V2.4.0

---
+++ src/main/java/org/teasoft/exam/bee/prod/currency/CurrencyExam.java
@@ -0,0 +1,26 @@
+///*
+// * Copyright 2016-2022 the original author.All rights reserved.
+// * Kingstar(honeysoft@126.com)
+// * The license,see the LICENSE file.
+// */
+//
+//package org.teasoft.exam.bee.prod.currency;
+//
+//import org.teasoft.honey.util.currency.BeeFinancialCalculator;
+//
+///**
+// * @author Kingstar
+// * @since  1.11
+// */
+//public class CurrencyExam {
+//	
+//	public static void main(String[] args) {
+//		test();
+//	}
+//	
+//	public static void test() {
+//		System.out.println(BeeFinancialCalculator.calculate("(4+5)*(3-2)")); //9
+//		System.out.println(BeeFinancialCalculator.calculate("(4+5)*(3/2)")); //13.50
+//	}
+//
+//}

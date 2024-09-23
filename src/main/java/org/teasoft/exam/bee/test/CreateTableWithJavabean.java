86d0f180b5b8b506dbfb33ba757d7774f67865e6
Kingstar <honeysoft@126.com>
2024-09-24 00:30:22 +0800

    update/add test case or example

--- src/main/java/org/teasoft/exam/bee/test/CreateTableWithJavabean.java
+++ src/main/java/org/teasoft/exam/bee/test/CreateTableWithJavabean.java
@@ -6,6 +6,8 @@
 
 package org.teasoft.exam.bee.test;
 
+import org.teasoft.exam.bee.osql.annotation.moretable.Clazz1;
+import org.teasoft.exam.bee.osql.annotation.moretable.Student1;
 import org.teasoft.exam.bee.osql.commomid.Orders3;
 import org.teasoft.exam.bee.osql.commomid.OrdersIdString;
 import org.teasoft.exam.bee.osql.dialect.Noid2;
@@ -19,6 +21,7 @@ import org.teasoft.exam.bee.osql.entity.dynamic.LeafAlloc3;
 import org.teasoft.exam.bee.osql.entity.dynamic.Orders;
 import org.teasoft.exam.bee.osql.moretable.entity.Assigncourse;
 import org.teasoft.exam.bee.osql.moretable.entity.Assignexam;
+import org.teasoft.exam.bee.osql.special.ddl.Noid1;
 import org.teasoft.exam.bee.osql.special.primarykey.entity.CustomId;
 import org.teasoft.exam.bee.osql.special.primarykey.entity.TestPrimaryKey;
 import org.teasoft.exam.bee.osql.special.type.DateType2;
@@ -40,43 +43,56 @@ public class CreateTableWithJavabean {
 		try {
 			
 			Ddl.setDynamicParameter("version", ""); //default
-			Ddl.createTable(new Orders(), true);
+			Ddl.createTable( Orders.class, true);
 			
 			Ddl.setDynamicParameter("month", "_202007"); //要对应实现有该注解才有效
-			Ddl.createTable(new Orders(), true);
+			Ddl.createTable( Orders.class, true);
 			
-			Ddl.createTable(new TestUser(), true);
+			Ddl.setDynamicParameter("month", "1"); //要对应实现有该注解才有效
+			Ddl.createTable( Orders.class, true);
 			
-			Ddl.createTable(new LeafAlloc(), true);
+			Ddl.setDynamicParameter("month", "2"); //要对应实现有该注解才有效
+			Ddl.createTable( Orders.class, false);
+			
+			Ddl.setDynamicParameter("month", "3"); //要对应实现有该注解才有效
+			Ddl.createTable( Orders.class, false);
+			
+			Ddl.createTable( TestUser.class, true);
+			
+			Ddl.createTable( LeafAlloc.class, true);
 //
 			Ddl.setDynamicParameter("version", "2");
-			Ddl.createTable(new LeafAlloc3(), true);
+			Ddl.createTable( LeafAlloc3.class, true);
 			
 			Ddl.setDynamicParameter("version", "3");
-			Ddl.createTable(new LeafAlloc3(), true);
+			Ddl.createTable( LeafAlloc3.class, true);
 
-			Ddl.createTable(new Scores(), true);
-			Ddl.createTable(new Assignexam(), true);
-			Ddl.createTable(new Assigncourse(), true);
+			Ddl.createTable( Scores.class, true);
+			Ddl.createTable( Assignexam.class, true);
+			Ddl.createTable( Assigncourse.class, true);
 			
-			Ddl.createTable(new Clazz(), true);
-			Ddl.createTable(new Student(), true);
-			Ddl.createTable(new StudentHobby(), true);
+			Ddl.createTable( Clazz.class, true);
+			Ddl.createTable( Student.class, true);
+			Ddl.createTable( StudentHobby.class, true);
 //			
 //			Ddl.createTable(new TransferRecord(), true);
 //			
-			Ddl.createTable(new CustomId(), true);
+			Ddl.createTable( CustomId.class, true);
 			
-			Ddl.createTable(new Noid(), true);
-			Ddl.createTable(new Noid2(), true);
+			Ddl.createTable( Noid.class, true);
+			Ddl.createTable( Noid2.class, true);
+			Ddl.createTable( Noid1.class, true);
 //			Ddl.createTable(new CustomId(), true);
 //			Ddl.createTable(new CustomId(), true);
 			
-			Ddl.createTable(new Orders3(), true);
-			Ddl.createTable(new OrdersIdString(), true);
-			Ddl.createTable(new DateType2(), true);
-			Ddl.createTable(new TestPrimaryKey(), true);
+			Ddl.createTable( Orders3.class, true);
+			Ddl.createTable( OrdersIdString.class, true);
+			Ddl.createTable( DateType2.class, true);
+			Ddl.createTable( TestPrimaryKey.class, true);
 			
+			Ddl.createTable( Clazz1.class, true);
+			Ddl.createTable( Student1.class, true);
+			Ddl.createTable( StudentHobby.class, true);
 			
 		} catch (Exception e) {
 			Logger.error("In CreateTableWithJavabean3 (Exception):" + e.getMessage());

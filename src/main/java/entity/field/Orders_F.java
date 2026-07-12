f8bd20bc0c59578087078f46ec1bf23e1a3ca870
Kingstar <honeysoft@126.com>
2026-07-12 19:18:53 +0800

    update Orders_F

--- src/main/java/entity/field/Orders_F.java
+++ src/main/java/entity/field/Orders_F.java
@@ -3,23 +3,23 @@ package entity.field;
 /**
  * Relative field name for Javabean Orders
  * @author Honey
- * Create on 2024-09-13 00:11:08
+ * Create on 2026-07-12 18:12:54
  */
-public class Orders_F {
+public interface Orders_F {
 
-	private Orders_F() {}
-
-	public static final String id = "id";
-	public static final String userid = "userid";
-	public static final String orderid = "orderid";
-	public static final String name = "name";
-	public static final String total = "total";
-	public static final String createtime = "createtime";
-	public static final String remark = "remark";
-	public static final String sequence = "sequence";
-	public static final String abc = "abc";
-	public static final String updatetime = "updatetime";
-	public static final String ENTITY_NAME = "Orders";
-	public static final String TABLE_NAME = "orders";
-	public static final String ALL_NAMES = "id,userid,orderid,name,total,createtime,remark,sequence,abc,updatetime";
+	String id = "id";
+	String userid = "userid";
+	String orderid = "orderid";
+	String name = "name";
+	String total = "total";
+	String createtime = "createtime";
+	String remark = "remark";
+	String sequence = "sequence";
+	String abc = "abc";
+	String updatetime = "updatetime";
+	String detailInfo = "detailInfo";
+	String num = "num";
+	String ENTITY_NAME = "Orders";
+	String TABLE_NAME = "orders";
+	String ALL_NAMES = "id,userid,orderid,name,total,createtime,remark,sequence,abc,updatetime,detailInfo,num";
 }
